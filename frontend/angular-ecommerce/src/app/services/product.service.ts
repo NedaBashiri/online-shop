ff39e6e4a6c2a352d7ff9f8c4f3d88efb9779b19
neda <bashiri.neda72@gmail.com>
2023-09-06 22:46:19 +0430

    create angular service to call REST APIs

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
+import {Product} from "../common/product";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private url = 'http://localhost:8080/api/products';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getListProductsPaginate(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.url).pipe(
+      map(response=> response._embedded.products)
+    );
+
+  }
+}
+
+interface GetResponse{
+  _embedded: {
+    products: Product[];
+  }
 }
