44ceecce8ac4d92ee21365e7c5d2f47c4a391bbb
Neda bashiri <neda.bashiri.dev@gmail.com>
2023-09-07 23:10:08 +0430

    Install ng-bootstrap

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -10,6 +10,7 @@ import {ProductService} from "./services/product.service";
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 
 
@@ -33,7 +34,7 @@ import { ProductDetailsComponent } from './components/product-details/product-de
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-
+    NgbModule
   ],
 
   providers: [ProductService],
