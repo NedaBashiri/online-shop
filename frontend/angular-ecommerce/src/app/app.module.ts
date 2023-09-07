5064c23ef4b25ed682f4e2156beb99ea653ce106
Neda bashiri <neda.bashiri.dev@gmail.com>
2023-09-07 19:35:55 +0430

    create new component for product details

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import {ProductService} from "./services/product.service";
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 
 
@@ -21,6 +22,7 @@ import { SearchComponent } from './components/search/search.component';
     ProductListComponent,
     ProductCategoryMenuComponent,
     SearchComponent,
+    ProductDetailsComponent,
 
 
   ],
