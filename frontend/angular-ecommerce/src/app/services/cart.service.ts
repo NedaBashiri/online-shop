9f1c84f2bcfdd184c68720d61bba0bb3e8329cae
Neda bashiri <neda.bashiri.dev@gmail.com>
2023-09-08 18:36:06 +0430

    modify cat-service

--- frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -76,5 +76,15 @@ export class CartService {
       this.computeCartTotals();
     }
   }
+  remove(theCartItem: CartItem) {
+
+    const itemIndex = this.cartItems.findIndex( tempCartItem => tempCartItem.id === theCartItem.id );
+    
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
+    }
+  }
 
 }
