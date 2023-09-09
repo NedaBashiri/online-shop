a6a88cac0aece942891113b7c1d30d11900d64c1
Neda bashiri <neda.bashiri.dev@gmail.com>
2023-09-09 16:15:44 +0430

    generate our checkout component

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 
 
@@ -28,6 +29,7 @@ import { CartDetailsComponent } from './components/cart-details/cart-details.com
     ProductDetailsComponent,
     CartStatusComponent,
     CartDetailsComponent,
+    CheckoutComponent,
 
 
   ],
