eb54f4c78c5644efdee30abd543e416d101aa9a9
Neda bashiri <neda.bashiri.dev@gmail.com>
2023-09-12 18:20:25 +0430

    create controller

---
+++ backend/ecommerce/src/main/java/com/neda/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,30 @@
+package com.neda.ecommerce.controller;
+
+
+import com.neda.ecommerce.dto.Purchase;
+import com.neda.ecommerce.dto.PurchaseResponse;
+import com.neda.ecommerce.service.CheckoutService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+
+}
+
+
