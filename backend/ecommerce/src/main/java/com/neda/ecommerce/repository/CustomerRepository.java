319e84ee5f6b919288f30de90a7401f6fbe59d0e
Neda bashiri <neda.bashiri.dev@gmail.com>
2023-09-12 18:12:38 +0430

    create customer-repository

---
+++ backend/ecommerce/src/main/java/com/neda/ecommerce/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.neda.ecommerce.repository;
+
+import com.neda.ecommerce.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
