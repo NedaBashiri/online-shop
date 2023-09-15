fe8c20f11be0e2c775b377b615256c983455b195
Neda bashiri <neda.bashiri.dev@gmail.com>
2023-09-15 18:31:48 +0430

    set up app configuration for openID connect

---
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+  oidc: {
+    clientId: '0oaba0kkm6GT28XJG5d7',
+    issuer: 'https://dev-02597101.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+
+}
