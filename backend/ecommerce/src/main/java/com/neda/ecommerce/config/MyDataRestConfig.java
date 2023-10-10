799af59e610e1e9cc39faa5196f42b751755d3cb
Neda bashiri <neda.bashiri.dev@gmail.com>
2023-10-10 11:55:11 +0330

    Update configs to make order-repository REST API read only

--- backend/ecommerce/src/main/java/com/neda/ecommerce/config/MyDataRestConfig.java
+++ backend/ecommerce/src/main/java/com/neda/ecommerce/config/MyDataRestConfig.java
@@ -1,10 +1,7 @@
 package com.neda.ecommerce.config;
 
 
-import com.neda.ecommerce.entity.Country;
-import com.neda.ecommerce.entity.Product;
-import com.neda.ecommerce.entity.ProductCategory;
-import com.neda.ecommerce.entity.State;
+import com.neda.ecommerce.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -42,6 +39,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
         disableHttpMethods(Country.class, config, theUnsupportedActions);
         disableHttpMethods(State.class, config, theUnsupportedActions);
+        disableHttpMethods(Order.class, config, theUnsupportedActions);
 
         // call an internal helper method
         exposeIds(config);
