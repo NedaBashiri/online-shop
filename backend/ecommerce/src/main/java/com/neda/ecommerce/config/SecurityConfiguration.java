71ed0de10d4b6c91652b567f32542a2de3af1718
Neda bashiri <neda.bashiri.dev@gmail.com>
2023-10-10 16:20:09 +0330

    update spring security configuration class

--- backend/ecommerce/src/main/java/com/neda/ecommerce/config/SecurityConfiguration.java
+++ backend/ecommerce/src/main/java/com/neda/ecommerce/config/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.neda.ecommerce.config;
 
+import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 
 @Configuration
 public class SecurityConfiguration {
@@ -18,8 +21,14 @@ public class SecurityConfiguration {
                 .oauth2ResourceServer()
                 .jwt();
 
-       
+
         http.cors();
+
+        http.setSharedObject(ContentNegotiationStrategy.class,
+                new HeaderContentNegotiationStrategy());
+        
+        Okta.configureResourceServer401ResponseBody(http);
+
         return http.build();
     }
 }
